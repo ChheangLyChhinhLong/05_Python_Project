03b22c39905cb28b2eb3a99287a7fdaa13dfd87d
ChhinhLong ChheangLy <chhinhlong.dev@gmail.com>
2026-07-31 08:18:00 +0700

    Add python list example with positive and negative indexing
    Created 1_python_list.py to demonstrate list creation, elements, and positive/negative index lookups for Build Bright University Python lab.

---
+++ Lab/Basic_Python/1_python_list.py
@@ -0,0 +1,12 @@
+bbu_capuses = [
+    "PP", "TK", "SH", "SR", "BB" , "BMC", "RK", "ST", "TB"
+]
+
+
+
+# positive and negative index
+# positive index: >=0
+print (f"(Index 5:{bbu_capuses}")
+
+# negative index: <0
+print(f"fIndex -4:{bbu_capuses[-4]}")
